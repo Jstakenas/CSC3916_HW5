02f079af251a09996bc23fc9d891d2381a0598c5
Jordan <jordan.stakenas@enosix.com>
2021-04-18 12:59:41 -0600

    semi-functional website

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import MovieHeader from './components/movieheader';
+import MovieList from "./components/movielist";
 import Authentication from './components/authentication';
+import Movie from './components/movie';
 import {HashRouter, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './stores/store';
@@ -13,9 +15,9 @@ function App() {
         <HashRouter>
           <div>
             <MovieHeader />
-            <Route exact path="/" render={()=><p />}/>
-            <Route exact path="/movielist" render={()=><p />}/>
-            <Route exact path="/movie/:movieId" render={()=><p />}/>
+            <Route exact path="/" render={()=><MovieList />}/>
+            <Route exact path="/movielist" render={()=><MovieList />}/>
+            <Route exact path="/movie/:title" render={()=><Movie />}/>
             <Route path="/signin" render={()=><Authentication />}/>
           </div>
         </HashRouter>
